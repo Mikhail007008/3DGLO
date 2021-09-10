e836d61f82781ed20c9e661c7f64bff02be668f9
Mikhail007008 <kasseta136@mail.ru>
2021-09-10 22:14:09 +0300

    build(3DGLO): did timer, added new conditions

---
+++ script/script.js
@@ -0,0 +1,42 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+	'use strict';
+
+	//Timer
+	function countTimer(deadline){
+
+		let timerHours = document.querySelector('#timer-hours'),
+			timerMinutes = document.querySelector('#timer-minutes'),
+			timerSeconds = document.querySelector('#timer-seconds');
+
+		function getTimeRemaining(){
+			let	dateStop = new Date(deadline).getTime(),
+				dateNow = new Date().getTime(),
+				timeRemaining = (dateStop - dateNow) / 1000,
+				seconds = Math.floor(timeRemaining % 60),
+				minutes = Math.floor((timeRemaining / 60) % 60),
+				hours = Math.floor(timeRemaining / 60 / 60);
+
+			return {dateStop, dateNow, timeRemaining, hours, minutes, seconds};
+		}
+
+		function updateClock(){
+			let timer = getTimeRemaining();
+
+			if(timer.timeRemaining > 0){
+				timerHours.textContent = ('0' + timer.hours).slice(-2);
+				timerMinutes.textContent = ('0' + timer.minutes).slice(-2);
+				timerSeconds.textContent = ('0' + timer.seconds).slice(-2);	
+						
+			}else if(timer.dateStop < timer.dateNow){
+				timerHours.textContent = '00';
+				timerMinutes.textContent = '00';
+				timerSeconds.textContent = '00';
+			}
+		}
+
+		setInterval(updateClock, 1000);
+	}
+
+	countTimer('11 september 2021');
+});
